b0398816aa7a1a0fccd3046627630ffa7c1311f2
Augusto Goulart <augusto@goulart.me>
2021-02-04 23:30:15 -0300

    Action can delete items from table

--- client/src/components/table/table.jsx
+++ client/src/components/table/table.jsx
@@ -1,7 +1,7 @@
 import 'antd/dist/antd.css';
 import {BuildColumns} from "./columns";
 import {Component} from 'react';
-import {Button, Dropdown, message, Space, Table} from 'antd';
+import {Button, Dropdown, Space, Table} from 'antd';
 import {DownOutlined} from '@ant-design/icons';
 import {Overlay} from "../dropdown-overlay/Overlay";
 
@@ -10,12 +10,17 @@ class ModelTable extends Component {
   state = {
     filteredInfo: null,
     sortedInfo: null,
-    data: null
+    data: null,
+    selectedRowKeys: []
   };
 
   columns() {
     const { sortedInfo, filteredInfo } = this.state;
     return BuildColumns(sortedInfo, filteredInfo)
+  }
+
+  onRowSelectionChange = (selectedRowKeys) => {
+    this.setState({selectedRowKeys})
   }
 
   handleChange = (pagination, filters, sorter) => {
@@ -45,8 +50,11 @@ class ModelTable extends Component {
     });
   };
 
-  onMenuItemClick({key}) {
-    message.info(`User ${key} was deleted`)
+  onMenuItemClick= () => {
+    const {selectedRowKeys, data} = this.state
+
+    const updatedDate = data.filter(item => !selectedRowKeys.includes(item.key))
+    this.setState({data: updatedDate})
   }
 
   componentDidMount() {
@@ -56,8 +64,11 @@ class ModelTable extends Component {
   }
 
   render() {
-    const { data } = this.state;
-
+    const { data, selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onRowSelectionChange
+    }
     return (
       <>
         <Space style={{ marginBottom: 16 }}>
@@ -68,7 +79,11 @@ class ModelTable extends Component {
           <Button onClick={this.clearFilters}>Clear filters</Button>
           <Button onClick={this.clearAll}>Clear filters and sorters</Button>
         </Space>
-        <Table rowSelection={{type: "checkbox"}} columns={this.columns()} dataSource={data} onChange={this.handleChange} />
+        <Table
+          rowSelection={rowSelection}
+          columns={this.columns()}
+          dataSource={data}
+          onChange={this.handleChange} />
       </>
     );
   }
