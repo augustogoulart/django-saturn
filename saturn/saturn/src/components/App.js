31594cc97bc9023692771fa9377a4162db842ee6
Augusto Goulart <augusto@goulart.me>
2021-02-21 22:58:50 -0300

    Migrating from CRA: Setup scss loaders

--- saturn/saturn/src/components/App.js
+++ saturn/saturn/src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+import "./styles.scss"
 
 const App = () => (
   <div>
-    <h1>React and Django!</h1>
+    <h1>Cosmic Admin</h1>
   </div>
 )
 
