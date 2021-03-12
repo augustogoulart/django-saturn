b1d9a40f9aa67525dd99e1915d61ed87b7510f0a
Augusto Goulart <augusto@goulart.me>
2021-03-12 00:39:27 -0300

    Simplify the register process - take 2

--- sandbox/admin.py
+++ sandbox/admin.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
 from saturn.admin import site as saturn_admin
-from saturn.options import SaturnAdminModel
 from .models import DummyUser
 
 # Register to Django Admin
-admin.site.register(DummyUser)
-
+admin.site.register([DummyUser])
 
 # Register to Saturn Admin
-saturn_admin.register(DummyUser)
-saturn_admin.register(User)
-saturn_admin.register(Group)
-
+saturn_admin.register([DummyUser, User, Group])
