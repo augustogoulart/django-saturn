7827021f58ea1fe04af55eee1c7b950fe9221e8d
Augusto Goulart <augusto@goulart.me>
2021-03-15 00:13:28 -0300

    Name -> Username

--- sandbox/models.py
+++ sandbox/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DummyUser(models.Model):
-    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
