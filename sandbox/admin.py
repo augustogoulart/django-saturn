6af56144eb59c7b4f7235588595893486a3e2c95
Augusto Goulart <augusto@goulart.me>
2021-03-18 22:58:12 -0300

    Add more models for testing

--- sandbox/admin.py
+++ sandbox/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
 from saturn.admin import site as saturn_admin
-from .models import DummyUser
+from .models import DummyUser, DummyProduct
 
 # Register to Django Admin
-admin.site.register([DummyUser])
+admin.site.register([DummyUser, DummyProduct])
 
 # Register to Saturn Admin
-saturn_admin.register([DummyUser, User, Group])
+saturn_admin.register([User, Group, DummyUser, DummyProduct])
--- sandbox/models.py
+++ sandbox/models.py
@@ -6,3 +6,11 @@ class DummyUser(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class DummyProduct(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
