6c5f7d7b0baf233ab4d279b226b3d07b79f0e87c
Augusto Goulart <augusto@goulart.me>
2021-03-30 21:41:39 -0300

    Show empty element when no model is registered

--- saturn/frontend/pages/homepage/homepage.jsx
+++ saturn/frontend/pages/homepage/homepage.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import {Table} from 'antd';
-import {Link} from 'react-router-dom';
+import {Empty, Table} from 'antd';
 
 import './homepage.scss';
 
 function HomePage() {
-  const app_list = []
   return (
-    <>
-      {
-        app_list.map(
-          app =>
-            <Table
-              style={{"padding": "3vh"}}
-              key={app.app_label}
-              rowKey={'name'}
-              columns={[{
-                title: app.name,
-                key: app.app_url,
-                dataIndex: "name",
-                render: (name, row) => <Link to={row['frontend_url']}>{name}</Link>
-              }]}
-              dataSource={app.models}
-              pagination={false}/>)
-      }
-    </>
+   <Empty description={"No Models Registered"} image={Empty.PRESENTED_IMAGE_SIMPLE}/>
   )
 }
 
